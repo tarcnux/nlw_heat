06bbff053c5c731e884d735046c2ac2556d9a2f5
Tarcisio Nunes <github@tarcnux.com.br>
2021-10-21 20:47:45 -0300

    Sass configuration

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,10 @@
+import styles from './App.module.scss'
 
 export function App() {
   
-
   return (
-  <h1>Alo Ha Mundão!</h1>
+    <main className={styles.contentWrapper}>
+      <h1>Alo HA Mundo Velho!</h1>
+    </main>
   )
 }
